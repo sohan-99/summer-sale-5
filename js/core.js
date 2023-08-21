e1f562fc77281e4c1d03701d6aadbf01e5c8cccf
sohan-99 <sohan75632@gmail.com>
2023-08-21 13:35:28 +0600

    3rd

--- js/core.js
+++ js/core.js
@@ -1,69 +1,106 @@
-document.getElementById('card1').addEventListener('click', function(){
-    // console.log('card 1 clicked');
-    const card1Prise = document.getElementById('s1c1')
-    const card1PriseAmount = card1Prise.innerText
-    // console.log(card1PriseAmount);
-   
-    //
-    const card1item = document.getElementById('s1c1t')
-    const card1text = card1item.innerText
-    
-    // console.log(card1text);
-    // set 1
-    const textArea = document.getElementById('textArea1')
-    const textSpace1 = textArea.innerText
-    textArea.innerText = card1text
-   
-    // set2 
-    const textAarea = document.getElementById('textArea2')
-    const textSpace2 = textAarea.innerText
-    textAarea.innerText = card1PriseAmount
-    
-})
-//card-2
-document.getElementById('card2').addEventListener('click', function(){
-    const card2prise =document.getElementById('s1c2')
-    const card2PriseAmount =card2prise.innerText
-    //
-    const card2item = document.getElementById('s1c2t')
-    const card2text = card2item.innerText
-    // set 
-    const textAreac2 = document.getElementById('textArea1')
-    const textspacec2 = textAreac2.innerText
-    textAreac2.innerText = card2text
-    // set 
-    const textAareac2 = document.getElementById('textArea2')
-    const textspace2c2 = textAareac2.innerText
-    textAareac2.innerText = card2PriseAmount
+document.addEventListener('DOMContentLoaded', function () {
+    const selectedCards = []; // Array to store selected cards
 
-})
-document.getElementById('card3').addEventListener('click', function(){
-    const card3prise =document.getElementById('s1c3')
-    const card3PriseAmount =card3prise.innerText
-    //
-    const card3item = document.getElementById('s1c3t')
-    const card3text = card3item.innerText
-    // set 
-    const textAreac3 = document.getElementById('textArea1')
-    const textspacec3 = textAreac3.innerText
-    textAreac3.innerText = card3text
-    // set 
-    const textAareac3 = document.getElementById('textArea2')
-    const textspace2c3 = textAareac3.innerText
-    textAareac3.innerText = card3PriseAmount
+    function updateSelectedCards() {
+        let totalAmount = 0;
 
-})
+        const cardCounts = {}; // Object to track card quantities
+
+        for (const card of selectedCards) {
+            totalAmount += card.price * card.quantity;
+            
+            cardCounts[card.name] = cardCounts[card.name] ? cardCounts[card.name] + 1 : 1;
+            discount =(totalAmount * 20/100)
+            total =totalAmount-(totalAmount * 20/100)
 
 
+        }
 
-// function titleGet(){
-//     const title = getInputCard('s1c1t')
-//     console.log(title);
-// }
+        const textArea1 = document.getElementById('textArea1');
+        textArea1.innerHTML = ''; // Clear the list before updating
 
-// // get input 
-// function getInputCard(fildId){
-// const inputCardValue =document.getElementById(fildId)
-// const Card1text =  inputCardValue.innerText
-// return Card1text
-// }
+        for (const card of selectedCards) {
+            const cardLine = document.createElement('div');
+
+            cardLine.innerText = `${card.name} x${card.quantity}`;
+            textArea1.appendChild(cardLine);
+        }
+
+        const textArea2 = document.getElementById('textArea2');
+        textArea2.innerText = ` ${totalAmount}TK`;
+        const textArea3 = document.getElementById('textArea3');
+        textArea3.innerText = ` ${discount}TK`;
+        const textArea4 = document.getElementById('textArea4');
+        textArea4.innerText = ` ${total}TK`;
+    }
+
+    function cardClickHandler(cardElement, cardPrice, cardText) {
+        const cardName = cardText.innerText;
+        const cardPriceAmount = parseFloat(cardPrice.innerText);
+
+        const existingCard = selectedCards.find(card => card.name === cardName);
+
+        if (existingCard) {
+            existingCard.quantity++;
+        } else {
+            selectedCards.push({ name: cardName, price: cardPriceAmount, quantity: 1 });
+        }
+
+        updateSelectedCards();
+    }
+    // card 1
+    document.getElementById('card1').addEventListener('click', function () {
+        const card1Prise = document.getElementById('s1c1');
+        const card1item = document.getElementById('s1c1t');
+        cardClickHandler(this, card1Prise, card1item);
+    });
+    // card -2 
+    document.getElementById('card2').addEventListener('click', function () {
+        const card2prise = document.getElementById('s1c2');
+        const card2item = document.getElementById('s1c2t');
+        cardClickHandler(this, card2prise, card2item);
+    });
+    // card-3
+    document.getElementById('card3').addEventListener('click', function () {
+        const card3prise = document.getElementById('s1c3');
+        const card3item = document.getElementById('s1c3t');
+        cardClickHandler(this, card3prise, card3item);
+    });
+    // card-4
+    document.getElementById('card4').addEventListener('click', function () {
+        const card4prise = document.getElementById('s2c4');
+        const card4item = document.getElementById('s2c4t');
+        cardClickHandler(this, card4prise, card4item);
+    });
+
+    // card-5
+    document.getElementById('card5').addEventListener('click', function () {
+        const card5prise = document.getElementById('s2c5');
+        const card5item = document.getElementById('s2c5t');
+        cardClickHandler(this, card5prise, card5item);
+    });
+    // card-6
+    document.getElementById('card6').addEventListener('click', function () {
+        const card6prise = document.getElementById('s2c6');
+        const card6item = document.getElementById('s2c6t');
+        cardClickHandler(this, card6prise, card6item);
+    });
+     // card-7
+     document.getElementById('card7').addEventListener('click', function () {
+        const card7prise = document.getElementById('s3c7');
+        const card7item = document.getElementById('s3c7t');
+        cardClickHandler(this, card7prise, card7item);
+    });
+     // card-8
+     document.getElementById('card8').addEventListener('click', function () {
+        const card8prise = document.getElementById('s3c8');
+        const card8item = document.getElementById('s3c8t');
+        cardClickHandler(this, card8prise, card8item);
+    });
+     // card-9
+     document.getElementById('card9').addEventListener('click', function () {
+        const card9prise = document.getElementById('s3c9');
+        const card9item = document.getElementById('s3c9t');
+        cardClickHandler(this, card9prise, card9item);
+    });
+});
